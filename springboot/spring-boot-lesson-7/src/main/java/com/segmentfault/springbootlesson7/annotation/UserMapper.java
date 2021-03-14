20bcef92d05d9a05bd0b50780dfc7ee3b86ed9fb
Gcdd1993 <mobile17602526128@gmail.com>
2021-03-14 12:24:46 +0800

    commit all

--- springboot/spring-boot-lesson-7/src/main/java/com/segmentfault/springbootlesson7/annotation/UserMapper.java
+++ springboot/spring-boot-lesson-7/src/main/java/com/segmentfault/springbootlesson7/annotation/UserMapper.java
@@ -2,7 +2,6 @@ package com.segmentfault.springbootlesson7.annotation;
 
 
 import com.segmentfault.springbootlesson7.entity.User;
-import com.segmentfault.springbootlesson7.handler.DescriptionTypeHandler;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
@@ -15,7 +14,7 @@ public interface UserMapper {
             @Result(property = "id", column = "id", id = true),
             @Result(property = "name", column = "name"),
             @Result(property = "age", column = "age"),
-            @Result(property = "description", column = "desc", typeHandler = DescriptionTypeHandler.class),
+            @Result(property = "description", column = "desc"/*, typeHandler = DescriptionTypeHandler.class*/),
             @Result(property = "height", column = "height")
     })
     @Select("SELECT id,name,age,`desc`,height FROM user WHERE id = #{id}")
